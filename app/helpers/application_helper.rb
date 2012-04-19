c6fd8fb54c8235ba2fff9ac627fcb59783526e9b
Sarah Sheehan <ssheehan131@gmail.com>
2012-04-18 23:35:05 -0400

    updating titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  #define a base title and then add page title if it exists
+  def title
+  	base_title = "DIG4503 - Sarah Sheehan - Semester Project"
+
+  	if @title.nil?
+  		base_title
+  	else
+  		"#{base_title} | #{@title}"
+  	end
+  end
+
 end
